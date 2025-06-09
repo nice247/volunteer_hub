f33b9539f7309f3766dc1bf67f2a06be817595e3
nice247 <baha.osman249@gmail.com>
2025-06-09 10:50:53 +0300

    initial commit

---
+++ src/main/java/com/vho/activ/ActivApplication.java
@@ -0,0 +1,13 @@
+package com.vho.activ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivApplication.class, args);
+	}
+
+}
